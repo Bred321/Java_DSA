0cfbaa0aed2d6f1083d04ec6ecb1e56b14068d40
Bred321 <64319606+Bred321@users.noreply.github.com>
2025-07-31 14:28:43 +0700

    some more updates

--- Week5_Time_and_Space_Tradeoffs/P1.java
+++ Week5_Time_and_Space_Tradeoffs/P1.java
@@ -1,3 +1,17 @@
-public class P1 {
+import java.util.Arrays;
+import java.util.Random;
 
+public class P1 {
+    public static void main(String[] args){
+        int size = 1000000;
+        int[] arr = new int[size];
+
+        for(int i = 0; i < size; i++){
+            arr[i] = (int)(Math.random() * 100001);
+        }
+
+        // Sort the array
+        int[] builtinArr = arr;
+        Arrays.sort(builtinArr);
+    }
 }
