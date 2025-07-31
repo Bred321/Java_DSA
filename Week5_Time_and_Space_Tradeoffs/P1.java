7786dcc90e10014f3af1f4f9f342031d18529b79
Bred321 <64319606+Bred321@users.noreply.github.com>
2025-07-31 15:00:35 +0700

    updates

--- Week5_Time_and_Space_Tradeoffs/P1.java
+++ Week5_Time_and_Space_Tradeoffs/P1.java
@@ -3,15 +3,50 @@ import java.util.Random;
 
 public class P1 {
     public static void main(String[] args){
-        int size = 1000000;
-        int[] arr = new int[size];
+        int n = 1000000;
+        int[] arr = new int[n];
 
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < n; i++){
             arr[i] = (int)(Math.random() * 100001);
         }
 
         // Sort the array
         int[] builtinArr = arr;
         Arrays.sort(builtinArr);
+
+        // Counting sort
+        // 1) Nmax = largest number in the array + 1
+        // resultArr.size = n
+        // Nmax: the size of the frequency array
+        // 2) Going left to right => Increment the value in the frequency array
+        // 3) Calculate the accumulative array
+        // 4) Going from right to left of the input array, decrease first then add
+
+        // Step 1: Create the empty arrays
+        int n_max = 100001;
+        int[] freArr = new int[n_max];
+        int[] resultArr = new int[n];
+
+        // Step 2: Create the frequency array
+        for(int val: arr){
+            freArr[val] += 1;
+        }
+
+        // Step 3: Create the accumulative array
+        for(int i = 0; i < n_max; i++){
+            if(i == 0) continue;
+            freArr[i] += freArr[i - 1]; 
+        }
+
+        // Step 4: Create the result array
+        for(int i = n; i >= 0; i--){
+            int val = arr[i];
+            freArr[val] -= 1;
+            int pos = freArr[val];
+            resultArr[pos] = arr[i];
+        }
+
+
+
     }
 }
